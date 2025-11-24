4f296165207e6be28dd4b099ec9333e787a19d33
Thisen Ekanayake <thisenekanayake0330@gmail.com>
2025-11-24 19:05:41 +0530

    added frontend url

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ app = FastAPI(title="Brain Tumor Detection API (ResNet18 Binary Classifier)")
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],  # Frontend URLs
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://brainet-xi.vercel.app/"],  # Frontend URLs
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
