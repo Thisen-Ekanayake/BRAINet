70e5fdc856c1a4cb75dbbceaf7d4aa2bcb27f2d1
Thisen Ekanayake <thisenekanayake0330@gmail.com>
2026-01-01 13:48:28 +0530

    remove origin

--- app/main.py
+++ app/main.py
@@ -15,7 +15,6 @@ default_allowed_origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "https://brainet-xi.vercel.app",
-    "https://brainet.thisenekanayake.me"
 ]
 
 allowed_origins = [
